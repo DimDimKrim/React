bf31e1aaf555ae1bebce27a2334183e02cdae947
DimDimKrim <40217022+DimDimKrim@users.noreply.github.com>
2018-06-16 23:48:21 +0300

    Add files via upload
    Здесь находится код выводящий мою ФИО используя react.js
    
    Для показа использовать npm start

---
+++ index.js
@@ -0,0 +1,54 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+class Square extends React.Component {
+  render() {
+    return (
+      <button className="square">
+        {/* TODO */}
+      </button>
+    );
+  }
+}
+
+class Board extends React.Component {
+  renderSquare(i) {
+    return <Square />;
+  }
+
+  render() {
+    const status = 'Дмитрий Вощинин';
+
+   return (
+      <div>
+        <div className="status">{status}</div>
+        
+        
+        
+      </div>
+    );
+  }
+}
+
+class Game extends React.Component {
+  render() {
+    return (
+      <div className="game">
+        <div className="game-board">
+          <Board />
+        </div>
+        <div className="game-info">
+          <div>{/* status */}</div>
+          <ol>{/* TODO */}</ol>
+        </div>
+      </div>
+    );
+  }
+}
+
+// ========================================
+
+ReactDOM.render(
+  <Game />,
+  document.getElementById('root')
+);
